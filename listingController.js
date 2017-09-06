1beed449108c9e534cae268763c20ce21ed95384
Quark5 <31390389+Quark5@users.noreply.github.com>
2017-09-05 21:40:23 -0400

    Update listingController.js

--- listingController.js
+++ listingController.js
@@ -1,14 +1,28 @@
+
 angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
+	
+	$scope.booleanify = function() {
+	listings.map(function(entries) {
+		return entries.details = false;
+	});
+	};
 
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+		$scope.listings.push($scope.newRef);
+		$scope.newRef = undefined;
+	};
+    $scope.deleteListing = function(index) {
+		$scope.listings.splice(index,1);
+	};
+    $scope.showDetails = function(index) {
+		$scope.listings[index].details = !$scope.listings[index].details;
+	};
   }
 ]);
